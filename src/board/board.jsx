01fd9963aa4796e3bd70819eb6b3c8e4c283caf2
Chance Carr <ccarr16@byu.edu>
2025-10-01 11:44:11 -0600

    Create view component stubs

---
+++ src/board/board.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Board() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>board displayed here</div>
+    </main>
+  );
+}
---
+++ src/listBoards/listBoards.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function ListBoards() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>board list displayed here</div>
+    </main>
+  );
+}
