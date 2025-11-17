225eabd825913251ad93bee0f4c418c982857f6b
Chance Carr <ccarr16@byu.edu>
2025-11-17 16:36:29 -0700

    Add service directory and express endpoints

---
+++ service/index.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+let users = [];
+let boards = [];
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+// GetBoards
+apiRouter.get('/boards', verifyAuth, (_req, res) => {
+  res.send(boards);
+});
+
+// SubmitBoard
+apiRouter.post('/board', verifyAuth, (req, res) => {
+  boards = updateBoards(req.body);
+  res.send(boards);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+// updateBoards replaces a board's previous version if it exists, or adds a new board if not.
+function updateBoards(updatedBoard) {
+  let found = false;
+  for (const [i, oldBoard] of boards.entries()) {
+    if (updatedBoard.id == oldBoard.id) {
+      boards[i] = updatedBoard;
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    boards.push(updatedBoard);
+  }
+
+  if (boards.length > 10) {
+    boards.length = 10;
+  }
+
+  return boards;
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
